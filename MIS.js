d2a5dbe35757c56c9d60e7a000cb30d2778f9da1
Khushpreet_Singh <135731187+khushpree444@users.noreply.github.com>
2024-10-09 20:36:00 +0530

    Add files via upload

---
+++ MIS.js
@@ -0,0 +1,39 @@
+document.getElementById("chapter1-online").onclick = function() {
+    window.open("https://drive.google.com/file/d/1Ds6rS5ZvtuONVz7LPpBcVQuFmgSJCL9w/view");
+};
+
+document.getElementById("chapter1-offline").onclick = function() {
+    window.open("1-2_MIS.pdf"); // Replace with actual link
+};
+
+document.getElementById("chapter2-online").onclick = function() {
+    window.open("https://drive.google.com/file/d/1nP9PV9e0xHvueNUSkQahHAmqY7fM8mX8/view");
+};
+
+document.getElementById("chapter2-offline").onclick = function() {
+    window.open("3-6-7_MIS.pdf"); // Replace with actual link
+};
+
+document.getElementById("chapter3-online").onclick = function() {
+    window.open("https://drive.google.com/file/d/1XY4zJrmc3Z8COpjSThUohUbthcuIn6oH/view"); // Replace with actual link
+};
+
+document.getElementById("chapter3-offline").onclick = function() {
+    window.open("8-9_MIS.pdf"); // Replace with actual link
+};
+
+document.getElementById("chapter4-online").onclick = function() {
+    window.open("https://drive.google.com/file/d/1PvgmmUc1VV7dxvUegUt7dSc42ZUg3HvE/view"); // Replace with actual link
+};
+
+document.getElementById("chapter4-offline").onclick = function() {
+    window.open("3-6_MIS.pdf"); // Replace with actual link
+};
+
+document.getElementById("chapter5-online").onclick = function() {
+    window.open("https://drive.google.com/file/d/1yn263Sfbyno_pnFRT7Qbq0aUnBTGbTEr/view"); // Replace with actual link
+};
+
+document.getElementById("chapter5-offline").onclick = function() {
+    window.open("10-11_MIS.pdf"); // Replace with actual link
+};
