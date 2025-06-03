aa45351ff6b62b6e3394479c0565b33dc62992ba
akaal777 <khushpreetsingh9@outlook.com>
2025-06-03 22:26:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function redirectTo(url) {
-    document.body.style.transition = 'opacity 0.5s ease-out';
-    document.body.style.opacity = '0';
-    setTimeout(() => {
-        window.location.href = url;
-    }, 500);
-}
